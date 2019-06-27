f2cb65adbb57637a0f547cf83ed4f5a9499a37d2
Riddhi Bhave <bhave.riddhi@gmail.com>
2019-06-27 19:38:49 -0400

    new update

--- imports/ui/pages/AddCampaign.jsx
+++ imports/ui/pages/AddCampaign.jsx
@@ -6,6 +6,29 @@ import Campaigns from '../../api/campaigns.js';
 
 export default class AddCampaign extends Component {
 
+
+  var DatePicker = require("react-bootstrap-date-picker");
+
+  var App = React.createClass({
+  getInitialState: function(){
+    var value = new Date().toISOString();
+    return {
+      value: value
+    }
+  },
+  handleChange: function(value, formattedValue) {
+    this.setState({
+      value: value, // ISO String, ex: "2016-11-19T12:00:00.000Z"
+      formattedValue: formattedValue // Formatted String, ex: "11/19/2016"
+    });
+  },
+  componentDidUpdate: function(){
+    // Access ISO String and formatted values from the DOM.
+    var hiddenInputElement = document.getElementById("example-datepicker");
+    console.log(hiddenInputElement.value); // ISO String, ex: "2016-11-19T12:00:00.000Z"
+    console.log(hiddenInputElement.getAttribute('data-formattedvalue')) // Formatted String, ex: "11/19/2016"
+  },
+
     render() {
 
         return (
@@ -36,6 +59,11 @@ export default class AddCampaign extends Component {
                   <div class="valid-feedback">Valid.</div>
                   <div class="invalid-feedback">Please fill out this field</div>
               </div>
+              <FormGroup>
+                <ControlLabel>Label</ControlLabel>
+                <DatePicker id="example-datepicker" value={this.state.value} onChange={this.handleChange} />
+                <HelpBlock>Help</HelpBlock>
+              </FormGroup>;
               <form id="profileForm" class="form-horizontal">
                 <div className="form-group">
                   <label class="col-sm-3 control-label">End Date</label>
@@ -64,7 +92,7 @@ export default class AddCampaign extends Component {
                   <div class="valid-feedback">Valid.</div>
                   <div class="invalid-feedback">Please fill out this field</div>
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <Link to="/home">Submit</Link>
               </form>
             </div>
         )
