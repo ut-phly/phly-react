10d88426e368d5d256e4a95428c8357d1b9da8e5
Grace <grace.anconetani@gmail.com>
2019-07-03 00:17:22 -0500

    initial commit

--- imports/ui/pages/Landing.jsx
+++ imports/ui/pages/Landing.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { withHistory, Link } from 'react-router-dom';
 
 import {
+    Responsive,
     Menu,
     Container,
     Button,
     Segment,
     Grid,
-    Header
+    Header,
+    Icon
 } from 'semantic-ui-react';
 
 
@@ -18,11 +20,16 @@ export default class Landing extends Component {
         if (loggedIn) this.props.history.push('/home');
 
         return(
-            <div className="card mb-3">
+            <div>
 
-                <Menu fixed='top' inverted>
+                <Menu fixed='top' inverted color='blue'>
                     <Container>
-                        <Menu.Item header>PHLY</Menu.Item>
+                        <Menu.Item header
+                                    style={{
+                                        fontFamily: 'Nunito',
+                                        fontSize: '1.2em',
+                                        letterSpacing: '2px'}}>
+                                PHLY</Menu.Item>
                         <Menu.Item position='right'>
                             <Button inverted>Login</Button>
                             <Button style={{ marginLeft: '1.5em' }}>Sign Up</Button>
@@ -30,23 +37,74 @@ export default class Landing extends Component {
                     </Container>
                 </Menu>
 
-                <Segment style={{ padding: '7em'}}>
-                    <Grid container stackable verticalAlign='middle'>
-                        <Grid.Row>
-                            <Grid.Column width={8}>
-                                <Header as='h3' style={{ fontSize: '2em' }}>
-                                    Fundraise the right way
-                                </Header>
-                                <p style={{ fontSize: '1.33em' }}>
-                                    Phly makes fundraising easier and safer
-                                    for student organizations
-                                </p>
-                                <Button size='huge'>Get Started</Button>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Segment>
+                <Responsive>
+                    <Segment style={{ padding: '7.5em'}} vertical>
+                        <Grid container stackable verticalAlign='middle'>
+                            <Grid.Row>
+                                <Grid.Column width={12}>
+                                    <Header as='h1'
+                                            color='orange'
+                                            style={{
+                                                fontSize: '3.5em',
+                                                letterSpacing: '1.5px' }}>
+                                        Fundraise the right way
+                                    </Header>
+                                    <p style={{
+                                            fontSize: '1.66em',
+                                            color: '#2E5266',
+                                            letterSpacing: '.5px'}}>
+                                        Phly makes fundraising easier and safer
+                                        for student organizations
+                                    </p>
+                                    <Button size='big' color='orange'>
+                                        Get Started
+                                        <Icon name='right arrow'/>
+                                    </Button>
 
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Segment>
+
+                    <Segment inverted vertical
+                            color='blue'
+                            style={{ padding: '7.5em' }}>
+                        <Grid container stackable verticalAlign='middle' columns='equal'>
+                            <Grid.Row>
+                            <Header as='h1'
+                                    color='orange'
+                                    style={{
+                                        fontSize: '3.5em',
+                                        letterSpacing: '1.5px' }}>
+                                How we can help you
+                            </Header>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column textAlign='center'>
+                                    <Icon name='circle' style={{ paddingBottom: '1.5em' }} size='massive'/>
+                                    <p style={{ fontSize:'1.33em' }}>
+                                        Easily collect donations from students through
+                                        our integrations with Venmo, Cashapp, Paypal + More
+                                    </p>
+                                </Grid.Column>
+                                <Grid.Column textAlign='center'>
+                                    <Icon name='circle' style={{ paddingBottom: '1.5em' }} size='massive'/>
+                                    <p style={{ fontSize:'1.33em' }}>
+                                        Phly sends all donations directly to your nonprofit
+                                        partner, automatically and hassle free
+                                    </p>
+                                </Grid.Column>
+                                <Grid.Column textAlign='center'>
+                                    <Icon name='circle' style={{ paddingBottom: '1.5em' }} size='massive'/>
+                                    <p style={{ fontSize:'1.33em' }}>
+                                        The Phly Dashboard helps you track all of your
+                                        fundraising campaigns, all the time
+                                    </p>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Segment>
+                </Responsive>
             </div>
         )
     }
