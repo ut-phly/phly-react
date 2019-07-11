002368346209845950df1bfa6247bc3e16808eb6
Tara Kuruvilla <tara.kuruvilla@utexas.edu>
2019-07-11 00:24:41 -0500

    add insert to Mongo, not working because packages

--- imports/api/campaigns.js
+++ imports/api/campaigns.js
@@ -1,8 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check';
+import SimpleSchema from 'simpl-schema';
+//import { check } from 'meteor/check';
 
-export default Campaigns = new Mongo.Collection('campaigns');
+export const Campaigns = new Mongo.Collection('campaigns');
+
+const CampaignsSchema = new SimpleSchema({
+  name: {type: String},
+  startDate: {type: String},
+  endDate: {type: Date},
+  description: {type: String, optional: true},
+  userId: {type: String},
+})
+
+Campaigns.attachSchema(CampaignsSchema);
 
 if (Meteor.isServer) {
   Meteor.publish('campaigns', function() {
