3e7d19dc0a7218606179f0d707b3dc3241113cd2
Siddhi <siddhi@Riddhis-MacBook-Air-2.local>
2019-07-21 16:59:44 -0500

    changes

--- imports/api/campaigns.js
+++ imports/api/campaigns.js
@@ -1,25 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check';
+import SimpleSchema from 'simpl-schema';
+//import { check } from 'meteor/check';
 
-export default Campaigns = new Mongo.Collection('campaigns');
+export const Campaigns = new Mongo.Collection('campaigns');
 
-if (Meteor.isServer) {
-  Meteor.publish('campaigns', function() {
-      if (!this.userId) return this.ready();
-
-      var currentUser = this.userId;
-      return Campaigns.find({ userId: currentUser });
-  });
-}
-
-Meteor.methods({
-    'campaigns.insert'(name) {
-        check(name, String);
-
-        Campaigns.insert({
-            name: name,
-            userId: this.userId
-        });
-    }
+const CampaignsSchema = new SimpleSchema({
+ name: {type: String},
+ startDate: {type: String},
+ endDate: {type: Date},
+ description: {type: String, optional: true},
+ userId: {type: String},
 });
+
+Campaigns.attachSchema(CampaignsSchema);
+//Campaigns.schema = Schema.Campaigns;
