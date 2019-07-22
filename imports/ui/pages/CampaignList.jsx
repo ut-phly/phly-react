4d2316103977972331a345747ed75c4677647633
Siddhi <siddhi@Riddhis-MacBook-Air-2.local>
2019-07-21 23:26:19 -0500

    storing in database

--- imports/ui/pages/CampaignList.jsx
+++ imports/ui/pages/CampaignList.jsx
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { withHistory, Link } from 'react-router-dom';
 import Campaign from '../Campaign.jsx';
+import { createContainer } from 'meteor/react-meteor-data';
+//wasn't working when overrode original Campaign, so made Campaigns1, probabaly easy fix
+import { Campaigns1 } from '../../api/campaigns1.js';
+import PropTypes from 'prop-types';
+// import ListOfCampaigns from '..ListOfCampaigns.jsx';
+import { List } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 
-export default class CampaignList extends Component {
+export class CampaignList extends Component {
+    constructor(props){
+      super(props);
+
+      //setting up state
+      this.state = { campaigns: [] };
+    }
+
+
+    //problem might be with 'Campaign' here, because this part was
+    //different from the tutorial, but this code chunk was
+    //already written(i think by Grace)
     renderCampaigns() {
         let myCampaigns = this.props.campaigns;
-
         return myCampaigns.map((campaign) => {
-            return (
                 <Campaign
                     key={campaign._id}
                     campaign={campaign}
                 />
-            );
         });
     }
 
@@ -33,6 +48,9 @@ export default class CampaignList extends Component {
 
 
         return(
+          // we need to figure out how to display the campaign name from
+          // the renderCampaigns method. It might just be an easy html thing
+          // but I couldn't get it to show up. Maybe Grace knows
             <div className="card-body">
                 <form className="form-group">
                     <input
@@ -47,7 +65,7 @@ export default class CampaignList extends Component {
                 </ul>
                 <div className="form-group">
                   <small className="form-text text-muted">
-                      <Link to="/addCampaign">Create New Campaign</Link>
+                      <Link to="/addCampaign" className="waves-effect waves-light btn">Create New Campaign</Link>
                   </small>
                 </div>
             </div>
@@ -55,3 +73,18 @@ export default class CampaignList extends Component {
         )
     }
 }
+
+//saying that the prop types must be an array, or else dont accept it
+CampaignList.propTypes = {
+  campaigns: PropTypes.array.isRequired,
+};
+
+//subscribing to campaigns db
+export default createContainer(() => {
+  Meteor.subscribe('campaigns');
+  //sorts by name
+  return{
+    campaigns: Campaigns1.find({}, {sort: {name: 1}}).fetch(),
+
+  };
+}, CampaignList);
