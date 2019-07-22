1d1cf36c75cb16acd8fce40a548402c3dd7d29ac
Siddhi <siddhi@wireless-10-147-234-107.public.utexas.edu>
2019-07-22 13:00:32 -0500

    campaings1 file

---
+++ imports/api/campaigns1.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+//import { check } from 'meteor/check';
+
+export const Campaigns1 = new Mongo.Collection('campaigns1');
+
+const CampaignsSchema = new SimpleSchema({
+ name: {type: String},
+ startDate: {type: String},
+ endDate: {type: Date},
+ description: {type: String, optional: true},
+});
+
+Campaigns1.attachSchema(CampaignsSchema);
+//Campaigns.schema = Schema.Campaigns;
