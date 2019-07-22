da158996f1323ee47bb0d03fbee728736c60665a
Siddhi <siddhi@Riddhis-MacBook-Air-2.local>
2019-07-21 23:27:21 -0500

    changes

--- server/main.js
+++ server/main.js
@@ -1,11 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import Campaigns from '/imports/api/campaigns';
+import Campaigns1 from '/imports/api/campaigns1';
 
-import '../imports/api/campaigns.js';
 
 Meteor.startup(() => {
     if (Campaigns.find().count() === 0) {
         Campaigns.insert({ name: "Test campaign", userId: "kAoxxAEg4BmCga6Ys" });
         Campaigns.insert({ name: "Rose Test campaign", userId: "baYAsXZfkQBYy9uZM" });
     }
+
+    Meteor.publish('campaigns1', function() {
+      return Campaigns1.find({});
+    })
 });
