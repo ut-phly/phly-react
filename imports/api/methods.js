47e7431cf9338292f8fcc50b17c6288827cd06e7
Siddhi <siddhi@Riddhis-MacBook-Air-2.local>
2019-07-28 09:42:31 -0500

    updates

---
+++ imports/api/methods.js
@@ -0,0 +1,19 @@
+import { Meteor } from 'meteor/meteor';
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import SimpleSchema from 'simpl-schema';
+import { Campaigns1 } from './campaigns1.js';
+
+
+export const insert = new ValidatedMethod({
+  name: 'Campaigns1.methods.insert',
+  validate: new SimpleSchema({
+    name: { type: String },
+    startDate: { type: String },
+    endDate: { type: String },
+    description: { type: String },
+    user: { type: String}
+  }).validator(),
+  run(newCampaign) {
+    Campaigns1.insert(newCampaign)
+  }
+});
