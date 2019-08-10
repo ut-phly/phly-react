53946c97112638ce1ac88d90088d4185d8fa2304
Tara Kuruvilla <tara.kuruvilla@utexas.edu>
2019-08-10 12:09:14 -0500

    added public campaign pages

--- imports/ui/pages/CampaignPage.jsx
+++ imports/ui/pages/CampaignPage.jsx
@@ -9,13 +9,23 @@ export default class CampaignPage extends Component {
     super(props);
   }
 
+  //function to find specific campaign
+
 // add link to public campaign page here
 //figure out how to pass props for specific campaign to Campaign Page
   render() {
+    console.log(this.props);
+    console.log(this.props.location.state.campaignID);
     return (
         <div>
           <div className="card mb-3">
             <h2 className="card-header">Campaign Page</h2>
+            <Link to={{
+              pathname: `/public/${this.props.location.state.campaignID}`,
+              state: {
+                campaignID: this.props.location.state.campaignID
+              }
+            }}>Public Campaign Link</Link>
           </div>
         </div>
     );
--- imports/ui/pages/MainPage/CampaignItem.jsx
+++ imports/ui/pages/MainPage/CampaignItem.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { List, Header } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import CampaignPage from '../../pages/CampaignPage.jsx';
 
 export default class CampaignItem extends Component {
@@ -10,10 +10,20 @@ export default class CampaignItem extends Component {
   }
 
   render () {
+    // return (
+    //   <List.Item href={`/home/${this.props.campaign._id}`}>
+    //     {this.props.campaign.name}
+    //   </List.Item>
+    // );
     console.log(this.props.campaign._id);
     return (
-      <List.Item href={`/home/${this.props.campaign._id}`}>
-        {this.props.campaign.name}
+      <List.Item>
+        <Link to={{
+          pathname: `/home/${this.props.campaign._id}`,
+          state: {
+            campaignID: this.props.campaign._id
+          }
+        }}>{this.props.campaign.name}</Link>
       </List.Item>
     );
   }
