ad1885aac41644f09e9e8a5560698a4981faae7c
Tara Kuruvilla <tara.kuruvilla@utexas.edu>
2019-09-06 09:18:44 -0500

    button should show but not showing

--- imports/ui/pages/PublicCampaignPage.jsx
+++ imports/ui/pages/PublicCampaignPage.jsx
@@ -7,39 +7,86 @@ import { HTTP } from 'meteor/http';
 //import Campaigns from '../../api/campaigns.js';
 
 export default class CampaignPage extends Component {
-    render() {
+  render() {
+    Meteor.call('getClientToken', function(error, clientToken) {
+      if (error) {
+        console.log(error);
+      } else {
+        braintree.client.create({
+          authorization: clientToken
+        },function (clientErr, clientInstance) {
+          if (clientErr) {
+            console.error('Error creating client:', clientErr);
+            return;
+          }
+          // Create a Venmo component.
+          var venmoButton = document.getElementById('venmo-button');
+          braintree.venmo.create({
+            client: clientInstance
+          },function (venmoErr, venmoInstance) {
+            if (venmoErr) {
+              console.error('Error creating Venmo:', venmoErr);
+              return;
+            }
+            // Verify browser support before proceeding.
+            // if (!venmoInstance.isBrowserSupported()) {
+            //   console.log('Browser does not support Venmo');
+            //   return;
+            // }
 
-      Meteor.call('getClientToken', function(error, clientToken) {
-          if (error) {
-            console.log(error);
-          } else {
-            braintree.setup(clientToken, "dropin", {
-              container: "payment-form", // Injecting into <div id="payment-form"></div>
-              onPaymentMethodReceived: function (response) {
-                // When we submit the payment form,
-                // it'll create new customer first...
-                var nonce = response.nonce;
+            displayVenmoButton(venmoInstance);
+            if (venmoInstance.hasTokenizationResult()) {
+              venmoInstance.tokenize(function (tokenizeErr, payload) {
+                if (err) {
+                  handleVenmoError(tokenizeErr);
+                } else {
+                  handleVenmoSuccess(payload);
+                }
+              });
+              return;
+            }
+          });
+          function displayVenmoButton(venmoInstance) {
+            // Assumes that venmoButton is initially display: none.
+            venmoButton.style.display = 'block';
 
-                Meteor.call('btCreateCustomer', function(error, success) {
+            venmoButton.addEventListener('click', function () {
+              venmoButton.disabled = true;
+
+              venmoInstance.tokenize(function (tokenizeErr, payload) {
+                venmoButton.removeAttribute('disabled');
+
+              });
+            });
+          }
+        });
+        braintree.setup(clientToken, "dropin", {
+          container: "payment-form", // Injecting into <div id="payment-form"></div>
+          onPaymentMethodReceived: function (response) {
+            // When we submit the payment form,
+            // it'll create new customer first...
+            var nonce = response.nonce;
+
+            Meteor.call('btCreateCustomer', function(error, success) {
+              if (error) {
+                throw new Meteor.Error('customer-creation-failed');
+              } else {
+                // ... and when the customer is successfuly created,
+                // call method for creating a transaction (finally!)
+                let donation_amount = document.getElementById('donation_amount').value;
+                Meteor.call('createTransaction', nonce, donation_amount, function(error, success) {
                   if (error) {
-                    throw new Meteor.Error('customer-creation-failed');
+                    throw new Meteor.Error('transaction-creation-failed');
                   } else {
-                    // ... and when the customer is successfuly created,
-                    // call method for creating a transaction (finally!)
-                    let donation_amount = document.getElementById('donation_amount').value;
-                    Meteor.call('createTransaction', nonce, donation_amount, function(error, success) {
-                      if (error) {
-                        throw new Meteor.Error('transaction-creation-failed');
-                      } else {
-                        alert('Thank you for your donation!');
-                      }
-                    });
+                    alert('Thank you for your donation!');
                   }
                 });
               }
             });
           }
-      });
+        });
+      }
+    });
 
       return (
           <div>
@@ -53,6 +100,7 @@ export default class CampaignPage extends Component {
                       type="integer"
                       id="donation_amount"
                       placeholder="Amount"/>
+                    <div id="venmo-button"></div>
                     <div id="payment-form"></div>
                     <button type="submit" class="btn btn-success">Submit</button>
                   </div>
