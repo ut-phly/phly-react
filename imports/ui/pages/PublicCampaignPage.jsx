466e48415ee127839ef40037223083736bb56543
Tara Kuruvilla <tara.kuruvilla@utexas.edu>
2019-09-06 08:11:35 -0500

    paypal and credit card

--- imports/ui/pages/PublicCampaignPage.jsx
+++ imports/ui/pages/PublicCampaignPage.jsx
@@ -2,63 +2,62 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withHistory, Link } from 'react-router-dom';
 //import '../../api/payments.js';
+import { HTTP } from 'meteor/http';
 
 //import Campaigns from '../../api/campaigns.js';
 
 export default class CampaignPage extends Component {
-    componentWillMount() {
-      $.getScript('https://js.braintreegateway.com/v2/braintree.js');
-    }
     render() {
-      // Meteor.methods({
-      //   'getClientToken'(clientId) {
-      //     var generateToken = Meteor.wrapAsync(gateway.clientToken.generate, gateway.clientToken);
-      //     var options = {};
-      //
-      //     if (clientId) {
-      //       options.clientId = clientId;
-      //     }
-      //
-      //     var response = generateToken(options);
-      //     return response.clientToken;
-      //   }
-      // });
 
       Meteor.call('getClientToken', function(error, clientToken) {
-        var braintree = require('braintree');
-        if (error) {
-          console.log(error);
-          console.log(clientToken);
-        } else {
-          braintree.setup(clientToken, "dropin", {
-            container: "payment-form", // Injecting into <div id="payment-form"></div>
-            onPaymentMethodReceived: function (response) {
-              // When we submit the payment form,
-              // we'll receive a response that includes
-              // payment method nonce:
-              var nonce = response.nonce;
-              // Check the nonce printed in console after submitting the form.
-              console.log(nonce);
-            }
-          });
-        }
+          if (error) {
+            console.log(error);
+          } else {
+            braintree.setup(clientToken, "dropin", {
+              container: "payment-form", // Injecting into <div id="payment-form"></div>
+              onPaymentMethodReceived: function (response) {
+                // When we submit the payment form,
+                // it'll create new customer first...
+                var nonce = response.nonce;
+
+                Meteor.call('btCreateCustomer', function(error, success) {
+                  if (error) {
+                    throw new Meteor.Error('customer-creation-failed');
+                  } else {
+                    // ... and when the customer is successfuly created,
+                    // call method for creating a transaction (finally!)
+                    let donation_amount = document.getElementById('donation_amount').value;
+                    Meteor.call('createTransaction', nonce, donation_amount, function(error, success) {
+                      if (error) {
+                        throw new Meteor.Error('transaction-creation-failed');
+                      } else {
+                        alert('Thank you for your donation!');
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
       });
 
       return (
           <div>
-            // <head>
-            //     <script src="https://js.braintreegateway.com/v2/braintree.js"></script>
-            // </head>
             <div className="card mb-3">
               <h2 className="card-header">Public Campaign Page</h2>
               <form role="form">
-              <div class="row">
-                <div class="col-md-6 col-xs-12">
-                  <div id="payment-form"></div>
-                  <button type="submit" class="btn btn-success">Submit</button>
+                <div class="row">
+                  <div class="col-md-6 col-xs-12">
+                    <input
+                      className="form-control"
+                      type="integer"
+                      id="donation_amount"
+                      placeholder="Amount"/>
+                    <div id="payment-form"></div>
+                    <button type="submit" class="btn btn-success">Submit</button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
             </div>
           </div>
       );
--- server/main.js
+++ server/main.js
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/campaigns.js';
 
+// Define gateway variable
+var gateway;
+
 Meteor.startup(() => {
   var env;
   var braintree = require('braintree');
@@ -12,7 +15,8 @@ Meteor.startup(() => {
   }
   //console.log(env);
   // Initialize Braintree connection:
-  gateway = BrainTreeConnect({
+  //gateway = BrainTreeConnect({
+  gateway = braintree.connect({
     environment: env,
     publicKey:  '4qpbhj8rxyn4wb9k',
     privateKey: '4b8926c2bf26ec19a3435edfdbe4139b',
@@ -31,5 +35,50 @@ Meteor.methods({
 
     var response = generateToken(options);
     return response.clientToken;
+  },
+  btCreateCustomer: function(){
+    var user = Meteor.user();
+
+    var customerData = {
+      email: user.emails[0].address
+    };
+
+    // Calling the Braintree API to create our customer!
+    gateway.customer.create(customerData, function(error, response){
+      if (error){
+        console.log(error);
+      } else {
+        // If customer is successfuly created on Braintree servers,
+        // we will now add customer ID to our User
+        Meteor.users.update(user._id, {
+          $set: {
+            customerId: response.customer.id
+          }
+        });
+      }
+    });
+  },
+  createTransaction: function(nonceFromTheClient, donation_amount) {
+    var user = Meteor.user();
+
+    // Let's create transaction.
+    gateway.transaction.sale({
+      amount: donation_amount,
+      paymentMethodNonce: nonceFromTheClient, // Generated nonce passed from client
+      customer: {
+        id: user.customerId
+      },
+      options: {
+        submitForSettlement: true, // Payment is submitted for settlement immediatelly
+        storeInVaultOnSuccess: true // Store customer in Braintree's Vault
+      }
+    }, function (err, success) {
+      if (err) {
+        console.log(err);
+      } else {
+        // When payment's successful, add "paid" role to current user.
+        Roles.addUsersToRoles(user._id, 'paid', Roles.GLOBAL_GROUP)
+      }
+    });
   }
 });
