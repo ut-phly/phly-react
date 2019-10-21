9d3979c21b9f39e51c810f80734cfaf8c4e9c56a
Grace <grace.anconetani@gmail.com>
2019-10-20 22:43:31 -0500

    reverting

--- client/main.jsx
+++ client/main.jsx
@@ -3,7 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from '/imports/ui/App';
 
-import { renderRoutes } from '../imports/startup/routes.jsx'
+import { renderRoutes } from '../imports/startup/routes.jsx';
+import 'semantic-ui-less/semantic.less';
 
 Meteor.startup(() => {
     render(renderRoutes(), document.getElementById('react-target'));
